1f39bcc7a43288eb2eeb244861154393fd8e2542
Linus Viksten <linus94w@hotmail.com>
2016-12-16 10:27:33 +0100

    Två uppgifter färdiga

---
+++ DatabasTeknikOmtenta/DatabasTeknikOmtenta/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static string cns = @"server=(localdb)\MSSQLLocalDB;Database=NORTHWND;Trusted_Connection=Yes";
+        static void Main(string[] args)
+        {
+            //FreightByShipper();
+        }
+
+        public static void OrdersByCustomerCompanyName(string CompanyName)
+        {
+
+        }
+
+        public static void FreightByShipper()
+        {
+            string queryString =
+            @"SELECT ShipVia, Avg(Freight) AS AverageFreight, CompanyName
+              FROM Orders INNER JOIN
+              Shippers ON Orders.ShipVia = Shippers.ShipperID
+              GROUP BY ShipVia, CompanyName;";
+
+            using (SqlConnection connection = new SqlConnection(cns))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.Write("Sum: " + reader.GetValue(1));
+                    Console.WriteLine(" Company Name: " + reader.GetString(2));
+                    Console.WriteLine("=====================================");
+                }
+                reader.Close();
+
+            }
+        }
+
+        public static void CustomersPerSupplier()
+        {
+
+        }
+    }
+}
---
+++ DatabasTeknikOmtenta/NORTHWNDDatabase/dbo/Views/CustomersByEmployee.sql
@@ -0,0 +1,7 @@
+﻿CREATE VIEW [dbo].[CustomersByEmployee]
+	AS SELECT        Employees.FirstName, Employees.LastName, Customers.CompanyName, Customers.ContactName
+FROM            Customers INNER JOIN
+                         Orders ON Customers.CustomerID = Orders.CustomerID INNER JOIN
+                         Employees ON Orders.EmployeeID = Employees.EmployeeID
+
+GROUP BY	  Employees.FirstName, Employees.LastName, Customers.CompanyName, Customers.ContactName
